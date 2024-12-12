6728e2d5d4e5e124690967b5b6642ad01de335da
adam.ward <adam.ward@MU-C1-02.mullen.pvt.k12.co.us>
2024-12-12 15:19:41 -0700

    initial commit

---
+++ Hello World/src/HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld
+	{
+
+		public static void main(String[] args)
+			{
+				System.out.println("Hello World!");
+
+				System.out.println("Memento Mori, hang in there.");
+				
+				System.out.println("I miss Jt...");
+
+			}	
+	}
